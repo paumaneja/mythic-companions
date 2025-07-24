31708d980849865c59f1923841e236ba1d3e7de0
paumaneja <paumaneja@gmail.com>
2025-07-24 15:04:41 +0200

    update securityconfig to allow domain

--- backend/src/main/java/com/mythiccompanions/api/config/SecurityConfig.java
+++ backend/src/main/java/com/mythiccompanions/api/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "https://mythiccompanions.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
